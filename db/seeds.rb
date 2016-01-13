6dabca5b172588391fe1beac0e8166116c9af0bd
Toby Turnbull <turnbulltoby@gmail.com>
2016-01-13 13:04:26 +1100

    added seeds to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 Flight.destroy_all
-Reservation.destroy_all
 User.destroy_all
 Airplane.destroy_all
 
@@ -8,7 +7,9 @@ u2 = User.create :id => 8, :name => 'Toby', :email => 'toby@toby.com', :admin =>
 u3 = User.create :id => 9, :name => 'Rhi', :email => 'rhi@rhi.com', :admin => false
 
 a1 = Airplane.create :id => 4, :name => '007', :seat_row => 5, :seat_column => 5
+a2 = Airplane.create :id => 5, :name => '420', :seat_row => 3, :seat_column => 6
+a3 = Airplane.create :id => 6, :name => '900', :seat_row => 2, :seat_column => 8
 
-# r1 = Reservation.create :id => 7, :name => 'Alex', :email => 'alex@alex.com', :admin => false
-
-f1 = Flight.create :id => 1, :origin => 'SYD', :destination => 'MLB', :date => '2016-12-01', :airplane_id => 4
+f1 = Flight.create :flight_number => 10, :origin => 'SYD', :destination => 'MLB', :date => '2016-12-01 18:30', :airplane_id => 4
+f2 = Flight.create :flight_number => 11, :origin => 'DOG', :destination => 'CAT', :date => '2017-11-22', :airplane_id => 6
+f3 = Flight.create :flight_number => 12, :origin => 'POO', :destination => 'MAP', :date => '2013-02-9', :airplane_id => 5
